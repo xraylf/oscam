88e42a58323e3ab0a2fda320ad8992b913c9fdda
nx111 <gd.zhangdz@gmail.com>
2020-04-28 13:23:56 +0800

    enable camd33/constcw/serial/touchweb default.

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
 #define WEBIF 1
 #define WEBIF_LIVELOG 1
 #define WEBIF_JQUERY 1
-//#define TOUCH 1
+#define TOUCH 1
 //#define WITH_SSL 1
 #if defined(__linux__) || defined(__CYGWIN__)
 #define HAVE_DVBAPI 1
@@ -25,7 +25,7 @@
 //#define IPV6SUPPORT 1
 #define MODULE_MONITOR 1
 
-//#define MODULE_CAMD33 1
+#define MODULE_CAMD33 1
 #define MODULE_CAMD35 1
 #define MODULE_CAMD35_TCP 1
 #define MODULE_NEWCAMD 1
@@ -33,8 +33,8 @@
 #define MODULE_CCCSHARE 1
 #define MODULE_GBOX 1
 //#define MODULE_RADEGAST 1
-//#define MODULE_SERIAL 1
-//#define MODULE_CONSTCW 1
+#define MODULE_SERIAL 1
+#define MODULE_CONSTCW 1
 //#define MODULE_PANDORA 1
 //#define MODULE_GHTTP 1
 #define MODULE_SCAM 1
